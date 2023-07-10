c9dcafdaa1409ce6de616944229487ac9aa17fdf
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-07-10 16:22:50 -0300

    ok

--- main.js
+++ main.js
@@ -1,39 +1,61 @@
-
+//este ejercicio lo realizamos en la video llamada.
 function pedirArrayUsuario() {
+    // variable que aloje los datos,q ingrese el usuario
+    //pedirle al usuario que ingrese un numero o q para salir.
+    //array devuelve los numeros ingresado.
     let numeros=[];
-    let num ;
-    
+    let valorIngresado;
     do {
-        num = prompt("ingrese numero o q para salir");
-        let numeroParseado= parseInt(num);
-        if (numeroParseado !== "q"){
-            
-            numeros.push(num)
-        }
-            
-        } while (num !== "q"); 
-   return (numeros);
-}
+      valorIngresado= prompt("Ingresar un numero o q para salir")
+      if(valorIngresado !== "q") {
+        valorIngresado= parseInt(valorIngresado)
+        numeros.push(valorIngresado)
+      }
+    } while(valorIngresado !== "q")   
+    return numeros                          
+  } 
 
-    let resulatdo = pedirArrayUsuario();
-     console.log(resulatdo);
+
+
  
-
-
-
-
-
-function calcularNumeroMagico(arrayNum) {
-    let pares = array.filter (num => num % 2 === 0).reduce ((a,b)=> a + b ,0);
-    let impares = array.filter (num => num % 2 !==0).reduce((a,b)=> a + b,0);
-
+function calcularNumeroMagico(array){
+    //  numeros pares e impares 
+    //creo variable y su parametro ,devuleva el resultado
+    let numerosPares= array.filter(
+      function(numero){
+        return numero % 2 == 0 
+      }
+    );
+      console.log(numerosPares)
     
-    return pares > impares ? pares :impares -pares;
-}
- 
-
-function main() {
+    let numerosImpares= array.filter(
+      function(numero){
+        return numero % 2 !== 0 
+      }
+    ); 
+      console.log(numerosImpares)
+      //comparar los arrays 
+      //hacer la condicion  si hay pares, se sumen  y si hay impares se resten.
+      if(numerosPares.length > numerosImpares.length ){
+        let sumar=array.reduce(function(total, array){return total + array}, 0);
+        return sumar;
+      }else {
+        let restar=array.reduce(function(total, array){return total - array}, 0);
+        return restar;
+      }
+      
+  
+   }
+   
+//    console.log(calcularNumeroMagico(numeroArray))
+  
+  
+  function main() {
+   //la funcion pedirArrayUsuario,guarda el resultado
     let arrayUsuario = pedirArrayUsuario();
-    let numeroMagico = calcularNumeroMagico(arrayUsuario);
-    alert(numeroMagico)
-}
+    //pasa funcion y la guarda 
+    //lo muestra con un alerta.
+    calcularNumeroMagico(arrayUsuario);
+  }
+  
+  main()
